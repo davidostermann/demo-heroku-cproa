6057257edf2f2d2c25a168e7a66a825b850755f0
david <ostermann.david@gmail.com>
2018-03-05 16:02:58 +0100

    add app front

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.use('/', express.static(__dirname + '/front/dist'))
+
 app.get('*', (req, res) => {
-  res.send('Hello CPROA');
+  res.sendFile(__dirname + '/front/dist/index.html')
 })
+
+console.log('DIRNAME : ', __dirname)
 
 const port = process.env.PORT || 3000;
 
